58b9eb769f73ee8b4b9fe841e2143d0442274657
bala80300 <bala.teknotrait@gmail.com>
2023-08-10 15:51:25 +0530

    solved errors while pushing

--- AutomationScripts/src/main/java/org/example/Orangehrm.java
+++ AutomationScripts/src/main/java/org/example/Orangehrm.java
@@ -5,22 +5,29 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Main {
-    public static void main(String[] args) {
+import java.time.Duration;
+
+public class Orangehrm {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2)); // webpage takes time to load, so added an implicit wait
 
         // Login the webpage using correct credentials
-        WebElement userName = driver.findElement(By.xpath("/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/div[2]/input"));
+        WebElement userName = driver.findElement(By.xpath("//*[@name='username']"));
         userName.sendKeys("Admin");
+        Thread.sleep(2000);
 
-        WebElement password = driver.findElement(By.xpath("/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/div[2]/input"));
+        WebElement password = driver.findElement(By.xpath("//*[@name='password']"));
         password.sendKeys("admin123");
+        Thread.sleep(2000);
 
-        WebElement login_button = driver.findElement(By.xpath("/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button"));
+        WebElement login_button = driver.findElement(By.xpath("//*[@type='submit']"));
         login_button.click();
+        Thread.sleep(5000);
 
-driver.quit();
+
+    driver.quit();
     }
 }
